3f7b8a0efefb64b1595562fcfc3eabc17d4a200e
凌壹 <yufei.zyf@alibaba-inc.com>
2015-08-07 14:25:23 +0800

    fix help

--- tc-limit-upload.sh
+++ tc-limit-upload.sh
@@ -5,7 +5,7 @@ DEV=$(ip route show | head -1 | awk '{print $5}')
 # Help
 display_help() {
         echo "Usage: ./tc.sh [OPTION]"
-        echo -e "\tstart - Apply the tc limit"
+        echo -e "\tstart [speed](MB/s)- Apply the tc limit"
         echo -e "\tstop - Remove the tc limit"
         echo -e "\tstatus - Show status"
 }
@@ -27,13 +27,12 @@ start_tc() {
 
 	/sbin/tc qdisc add dev $DEV root handle 1: htb
 
-	/sbin/tc class add dev $DEV parent 1: classid 1:1  htb rate $1
+	/sbin/tc class add dev $DEV parent 1: classid 1:1  htb rate $1 ceil $1
          
 	/sbin/tc filter add dev $DEV parent 1:0 protocol ip u32 match ip dst 0.0.0.0/0 flowid 1:1
 }
 
 if [ $# == 1 ];   then
-    echo $#
     if [ "$1" == "-help" ];  then
         display_help
     elif [ "$1" == "stop" ];    then
@@ -42,6 +41,9 @@ if [ $# == 1 ];   then
         show_status
     elif [ "$1" == "start" ];   then
         start_tc 8mbps
+    else
+        echo "Invalid input"
+        echo "You can input -help for more information"  
     fi
 elif [ "$2" -gt 0 ] 2>/dev/null;   then
     start_tc $2mbps
@@ -49,15 +51,3 @@ else
     echo "Invalid input"
     echo "You can input -help for more information"    
 fi
-
-
-# first, clear previous tc qdisc
-#/sbin/tc qdisc del dev $DEV root
-
-#/sbin/tc qdisc add dev $DEV root handle 1: htb
-
-#/sbin/tc class add dev $DEV parent 1: classid 1:1  htb rate ${UPLINK}mbps
-
-#/sbin/tc filter add dev $DEV parent 1:0 protocol ip u32 match ip dst 0.0.0.0/0 flowid 1:1
-
-# first, clear previous tc qdisc
